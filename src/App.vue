72cb7d1cdf4fe82bbf95ad86608ef4d89332edb8
hugoriveros18 <hugoriverosfajardo@gmail.com>
2022-07-05 12:54:42 -0500

    Firts Version, challenge realized

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,291 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="main-container">
+    <section class="profile-container">
+      <div class="profile-section1">
+        <div class="profile-picture-container">
+          <img class="profile-picture" src="./assets/image-jeremy.png" alt="Jeremy">
+        </div>
+        <div class="profile-information">
+          <p class="report">Report for</p>
+          <p class="name">Jeremy Robson</p>
+        </div>
+      </div>
+      <div class="profile-section2">
+        <button class="period-selection" :class="{'selected': dailyActive,'not-selected': !dailyActive}" @click="dailyActivation">Daily</button>
+        <button class="period-selection" :class="{'selected': weeklyActive,'not-selected': !weeklyActive}" @click="weeklyActivation">Weekly</button>
+        <button class="period-selection" :class="{'selected': monthlyActive,'not-selected': !monthlyActive}" @click="monthlyActivation">Monthly</button>
+      </div>
+    </section>
+    <section class="cards-container">
+      <div class="cards-section section1">
+        <CardType :value="work" :periodValue="actualPeriod" :dailyActive="dailyActive" :weeklyActive="weeklyActive" :monthlyActive="monthlyActive"/>
+        <CardType :value="play" :periodValue="actualPeriod" :dailyActive="dailyActive" :weeklyActive="weeklyActive" :monthlyActive="monthlyActive"/>
+        <CardType :value="study" :periodValue="actualPeriod" :dailyActive="dailyActive" :weeklyActive="weeklyActive" :monthlyActive="monthlyActive"/>
+      </div>
+      <div class="cards-section section2">
+        <CardType :value="excercise" :periodValue="actualPeriod" :dailyActive="dailyActive" :weeklyActive="weeklyActive" :monthlyActive="monthlyActive"/>
+        <CardType :value="social" :periodValue="actualPeriod" :dailyActive="dailyActive" :weeklyActive="weeklyActive" :monthlyActive="monthlyActive"/>
+        <CardType :value="selfCare" :periodValue="actualPeriod" :dailyActive="dailyActive" :weeklyActive="weeklyActive" :monthlyActive="monthlyActive"/>
+      </div>
+    </section>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import CardType from './components/CardType.vue';
+import {  reactive, ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const jsonURL = require("./assets/data.json");
+const work = "work";
+const study = "study";
+const play = "play";
+const excercise = "excercise";
+const social = "social";
+const selfCare = "selfCare";
+
+let actualPeriod = reactive({
+  workTitle: jsonURL[0].title,
+  playTitle: jsonURL[1].title,
+  studyTitle: jsonURL[2].title,
+  excerciseTitle: jsonURL[3].title,
+  socialTitle: jsonURL[4].title,
+  selfCareTitle: jsonURL[5].title,
+
+  workActualHour: `${jsonURL[0].timeframes.daily.current}hrs`,
+  workActualHourWeekly: `${jsonURL[0].timeframes.weekly.current}hrs`,
+  workActualHourMonthly: `${jsonURL[0].timeframes.monthly.current}hrs`,
+  workPreviousHour: `Last day - ${jsonURL[0].timeframes.daily.previous}hrs`,
+  workPreviousHourWeekly: `Last week - ${jsonURL[0].timeframes.weekly.previous}hrs`,
+  workPreviousHourMonthly: `Last month - ${jsonURL[0].timeframes.monthly.previous}hrs`,
+
+  playActualHour: `${jsonURL[1].timeframes.daily.current}hrs`,
+  playActualHourWeekly: `${jsonURL[1].timeframes.weekly.current}hrs`,
+  playActualHourMonthly: `${jsonURL[1].timeframes.monthly.current}hrs`,
+  playPreviousHour: `Last day - ${jsonURL[1].timeframes.daily.previous}hrs`,
+  playPreviousHourWeekly: `Last week - ${jsonURL[1].timeframes.weekly.previous}hrs`,
+  playPreviousHourMonthly: `Last month - ${jsonURL[1].timeframes.monthly.previous}hrs`,
+  
+  studyActualHour: `${jsonURL[2].timeframes.daily.current}hrs`,
+  studyActualHourWeekly: `${jsonURL[2].timeframes.weekly.current}hrs`,
+  studyActualHourMonthly: `${jsonURL[2].timeframes.monthly.current}hrs`,
+  studyPreviousHour: `Last day - ${jsonURL[2].timeframes.daily.previous}hrs`,
+  studyPreviousHourWeekly: `Last week - ${jsonURL[2].timeframes.weekly.previous}hrs`,
+  studyPreviousHourMonthly: `Last month - ${jsonURL[2].timeframes.monthly.previous}hrs`,
+  
+  excerciseActualHour: `${jsonURL[3].timeframes.daily.current}hrs`,
+  excerciseActualHourWeekly: `${jsonURL[3].timeframes.weekly.current}hrs`,
+  excerciseActualHourMonthly: `${jsonURL[3].timeframes.monthly.current}hrs`,
+  excercisePreviousHour: `Last day - ${jsonURL[3].timeframes.daily.previous}hrs`,
+  excercisePreviousHourWeekly: `Last week - ${jsonURL[3].timeframes.weekly.previous}hrs`,
+  excercisePreviousHourMonthly: `Last month - ${jsonURL[3].timeframes.monthly.previous}hrs`,
+  
+  socialActualHour: `${jsonURL[4].timeframes.daily.current}hrs`,
+  socialActualHourWeekly: `${jsonURL[4].timeframes.weekly.current}hrs`,
+  socialActualHourMonthly: `${jsonURL[4].timeframes.monthly.current}hrs`,
+  socialPreviousHour: `Last day - ${jsonURL[4].timeframes.daily.previous}hrs`,
+  socialPreviousHourWeekly: `Last week - ${jsonURL[4].timeframes.weekly.previous}hrs`,
+  socialPreviousHourMonthly: `Last month - ${jsonURL[4].timeframes.monthly.previous}hrs`,
+  
+  selfCareActualHour: `${jsonURL[5].timeframes.daily.current}hrs`,
+  selfCareActualHourWeekly: `${jsonURL[5].timeframes.weekly.current}hrs`,
+  selfCareActualHourMonthly: `${jsonURL[5].timeframes.monthly.current}hrs`,
+  selfCarePreviousHour: `Last day - ${jsonURL[5].timeframes.daily.previous}hrs`,
+  selfCarePreviousHourWeekly: `Last week - ${jsonURL[5].timeframes.weekly.previous}hrs`,
+  selfCarePreviousHourMonthly: `Last month - ${jsonURL[5].timeframes.monthly.previous}hrs`,
+
+  
+});
+let dailyActive = ref(true);
+let weeklyActive = ref(false);
+let monthlyActive = ref(false);
+
+const dailyActivation = () => {
+  actualPeriod.value = "daily";
+  dailyActive.value = true;
+  weeklyActive.value = false;
+  monthlyActive.value = false;
 }
+const weeklyActivation = () => {
+  actualPeriod.valor1 = "weekly";
+  dailyActive.value = false;
+  weeklyActive.value = true;
+  monthlyActive.value = false;
+}
+const monthlyActivation = () => {
+  actualPeriod.value = "monthly";
+  dailyActive.value = false;
+  weeklyActive.value = false;
+  monthlyActive.value = true;
+}
+
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Rubik', sans-serif;
+}
+body {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  background-color: #223355;
+}
+main {
+  width: 1140px;
+  height: 540px;
+  display: flex;
+  gap: 30px;
+}
+.profile-container {
+  width: 23%;
+  height: 100%;
+  border-radius: 15px;
+  background-color: #1D204B;
+}
+.profile-section1 {
+  height: 368px;
+  padding-left: 30px;
+  border-radius: 15px;
+  background-color: #5746EA;
+}
+.profile-picture-container {
+  display: flex;
+  height: 30%;
+  padding: 30px 0 30px;
+}
+.profile-picture {
+  width: 90px;
+  height: 90px;
+  border: solid 4px white;
+  border-radius: 50%;
+}
+.profile-information {
+  display: flex;
+  flex-direction: column;
+}
+.report {
+  font-size: 15px;
+  font-weight: 400;
+  margin: 0;
+  color: #b5b3f0;
+}
+.name {
+  font-size: 40px;
+  font-weight: 300;
+  margin: 0;
+  margin-top: 5px;
+  color: #f1efff;
+}
+.profile-section2 {
+  height: 172px;
+  padding-left: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 25px;
+}
+.period-selection {
+  background: none;
+  font: inherit;
+  font-size: 18px;
+  font-weight: 300;
+	border: none;
+	padding: 0;
+  cursor: pointer;
+}
+.not-selected {
+  color: #676ca6;
+}
+.selected {
+  color: #fbfeff;
+}            
+.period-selection:hover {
+  color: #fbfeff;
+}
+.cards-container {
+  width: 77%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.cards-section {
+  height: 50%;
+  display: flex;
+}
+.section1 {
+  justify-content: space-between;
+}
+.section2 {
+  justify-content: space-between;
+  align-items: flex-end;
+}
+
+@media screen and (max-width: 425px) {
+  #app {
+    height: auto;
+
+  }
+  body {
+    width: 100%;
+  }
+  main {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    flex-direction: column;
+    gap: 0;
+    margin-bottom: 60px;
+  }
+  .profile-container {
+    width: 80%;
+    height: auto;
+    margin-top: 100px;
+    margin-bottom: 20px;
+  }
+  .profile-section1 {
+    display: flex;
+    gap: 15px;
+    align-items: center;
+    height: 130px;
+  }
+  .profile-picture-container {
+    height: 100%;
+    align-items: center;
+    padding: 0;
+  }
+  .profile-picture {
+    width: 60px;
+    height: 60px;
+  }
+  .name {
+    font-size: 22px;
+  }
+  .profile-section2 {
+    height: 80px;
+    padding-left: 0;
+    flex-direction: row;
+    align-items: center;
+  }
+  .cards-container {
+    width: 80%;
+    gap: 20px;
+    height: auto;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  .section1, .section2 {
+    width: 100%;
+    flex-direction: column;
+    gap: 20px;
+  }
 }
 </style>
---
+++ src/components/CardType.vue
@@ -0,0 +1,262 @@
+<template>
+  <div class="general-container" :class="backgroundCards">
+    <picture class="card-img-container">
+      <img class="card-img" :class="{'study-exception':isExcercise}" :src="urlImages" alt="">
+    </picture>
+    <div class="info-container">
+      <div class="title-container">
+        <p v-if="props.value == work">{{ props.periodValue.workTitle }}</p>
+        <p v-if="props.value == study">{{ props.periodValue.studyTitle }}</p>
+        <p v-if="props.value == play">{{ props.periodValue.playTitle }}</p>
+        <p v-if="props.value == excercise">{{ props.periodValue.excerciseTitle }}</p>
+        <p v-if="props.value == social">{{ props.periodValue.socialTitle }}</p>
+        <p v-if="props.value == selfCare">{{ props.periodValue.selfCareTitle }}</p>
+        <img src="../assets/icon-ellipsis.svg" alt="">
+      </div>
+      <div class="time-information-container">
+        <!-- ACTUAL HOUR INFORMATION -->
+        <p v-if="props.value == work && props.dailyActive" class="big">{{ props.periodValue.workActualHour }}</p>
+        <p v-if="props.value == work && props.weeklyActive" class="big">{{ props.periodValue.workActualHourWeekly }}</p>
+        <p v-if="props.value == work && props.monthlyActive" class="big">{{ props.periodValue.workActualHourMonthly }}</p>
+
+
+        <p v-if="props.value == study && props.dailyActive" class="big">{{ props.periodValue.studyActualHour }}</p>
+        <p v-if="props.value == study && props.weeklyActive" class="big">{{ props.periodValue.studyActualHourWeekly }}</p>
+        <p v-if="props.value == study && props.monthlyActive" class="big">{{ props.periodValue.studyActualHourMonthly }}</p>
+
+
+        <p v-if="props.value == play && props.dailyActive" class="big">{{ props.periodValue.playActualHour }}</p>
+        <p v-if="props.value == play && props.weeklyActive" class="big">{{ props.periodValue.playActualHourWeekly }}</p>
+        <p v-if="props.value == play && props.monthlyActive" class="big">{{ props.periodValue.playActualHourMonthly }}</p>
+
+
+        <p v-if="props.value == excercise && props.dailyActive" class="big">{{ props.periodValue.excerciseActualHour }}</p>
+        <p v-if="props.value == excercise && props.weeklyActive" class="big">{{ props.periodValue.excerciseActualHourWeekly }}</p>
+        <p v-if="props.value == excercise && props.monthlyActive" class="big">{{ props.periodValue.excerciseActualHourMonthly }}</p>
+
+
+        <p v-if="props.value == social && props.dailyActive" class="big">{{ props.periodValue.socialActualHour }}</p>
+        <p v-if="props.value == social && props.weeklyActive" class="big">{{ props.periodValue.socialActualHourWeekly }}</p>
+        <p v-if="props.value == social && props.monthlyActive" class="big">{{ props.periodValue.socialActualHourMonthly }}</p>
+
+
+        <p v-if="props.value == selfCare && props.dailyActive" class="big">{{ props.periodValue.selfCareActualHour }}</p>
+        <p v-if="props.value == selfCare && props.weeklyActive" class="big">{{ props.periodValue.selfCareActualHourWeekly }}</p>
+        <p v-if="props.value == selfCare && props.monthlyActive" class="big">{{ props.periodValue.selfCareActualHourMonthly }}</p>
+
+        <!-- PREVIOUS HOUR INFORMATION -->
+        <p v-if="props.value == work && props.dailyActive" class="small">{{ props.periodValue.workPreviousHour }}</p>
+        <p v-if="props.value == work && props.weeklyActive" class="small">{{ props.periodValue.workPreviousHourWeekly }}</p>
+        <p v-if="props.value == work && props.monthlyActive" class="small">{{ props.periodValue.workPreviousHourMonthly }}</p>
+        
+        
+        <p v-if="props.value == study && props.dailyActive" class="small">{{ props.periodValue.studyPreviousHour }}</p>
+        <p v-if="props.value == study && props.weeklyActive" class="small">{{ props.periodValue.studyPreviousHourWeekly }}</p>
+        <p v-if="props.value == study && props.monthlyActive" class="small">{{ props.periodValue.studyPreviousHourMonthly }}</p>
+        
+        
+        <p v-if="props.value == play && props.dailyActive" class="small">{{ props.periodValue.playPreviousHour }}</p>
+        <p v-if="props.value == play && props.weeklyActive" class="small">{{ props.periodValue.playPreviousHourWeekly }}</p>
+        <p v-if="props.value == play && props.monthlyActive" class="small">{{ props.periodValue.playPreviousHourMonthly }}</p>
+
+
+        <p v-if="props.value == excercise && props.dailyActive" class="small">{{ props.periodValue.excercisePreviousHour }}</p>
+        <p v-if="props.value == excercise && props.weeklyActive" class="small">{{ props.periodValue.excercisePreviousHourWeekly }}</p>
+        <p v-if="props.value == excercise && props.monthlyActive" class="small">{{ props.periodValue.excercisePreviousHourMonthly }}</p>
+        
+        <p v-if="props.value == social && props.dailyActive" class="small">{{ props.periodValue.socialPreviousHour }}</p>
+        <p v-if="props.value == social && props.weeklyActive" class="small">{{ props.periodValue.socialPreviousHourWeekly }}</p>
+        <p v-if="props.value == social && props.monthlyActive" class="small">{{ props.periodValue.socialPreviousHourMonthly }}</p>
+
+
+        <p v-if="props.value == selfCare && props.dailyActive" class="small">{{ props.periodValue.selfCarePreviousHour }}</p>
+        <p v-if="props.value == selfCare && props.weeklyActive" class="small">{{ props.periodValue.selfCarePreviousHourWeekly }}</p>
+        <p v-if="props.value == selfCare && props.monthlyActive" class="small">{{ props.periodValue.selfCarePreviousHourMonthly }}</p>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, computed, ref } from "vue";
+const work = "work";
+const study = "study";
+const play = "play";
+const excercise = "excercise";
+const social = "social";
+const selfCare = "selfCare";
+
+const props = defineProps({
+    value: String,
+    periodValue: Object,
+    dailyActive: Boolean,
+    weeklyActive: Boolean,
+    monthlyActive: Boolean
+})
+let isExcercise = ref(false)
+if(props.value == "excercise") {
+  isExcercise.value = true
+}
+let backgroundCards = computed(() => {
+  switch (props.value) {
+    case "work":
+      return ".general-container work-container";
+    case "study":
+      return ".general-container study-container";
+    case "play":
+      return ".general-container play-container";
+    case "social":
+      return ".general-container social-container";
+    case "excercise":
+      return ".general-container excercise-container";
+    case "selfCare":
+      return ".general-container selfCare-container"
+    default:
+      return ".general-container";
+  }
+})
+let urlImages = computed(() => {
+  switch (props.value) {
+    case "work":
+      return require("../assets/icon-work.svg");
+    case "study":
+      return require("../assets/icon-study.svg");
+    case "play":
+      return require("../assets/icon-play.svg");
+    case "social":
+      return require("../assets/icon-social.svg");
+    case "excercise":
+      return require("../assets/icon-exercise.svg");
+    case "selfCare":
+      return require("../assets/icon-self-care.svg");
+    default:
+      return require("../assets/icon-work.svg");
+  }
+});
+</script>
+
+<style scoped>
+.general-container {
+  width: 270px;
+  height: 254px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  border-radius: 16px;
+}
+.card-img-container {
+  height: 50px;
+  width: 100%;
+  display: grid;
+  justify-content: end;
+  overflow: hidden;
+}
+.card-img {
+  width: 80px;
+  right: 10px;
+  position: relative;
+  right: 20px;
+  bottom: 10px;
+  z-index: 0;
+}
+.study-exception {
+  bottom: 2px;
+}
+.info-container {
+  width: 100%;
+  height: 202px;
+  border-radius: 15px;
+  z-index: 1;
+  background-color: #1D204B;
+}
+.info-container:hover {
+  background-color: #34397b;
+  cursor: pointer;
+}
+.title-container {
+  box-sizing: border-box;
+  width: 100%;
+  height: 35%;
+  padding: 0 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.title-container p {
+  font-size: 20px;
+  color: #FFFFFF;
+}
+.title-container img {
+  width: 21px;
+  height: 5px;
+}
+.time-information-container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 10px 0 0 30px;
+  color: #FFFFFF;
+}
+.big {
+  margin: 0;
+  font-size: 50px;
+  font-weight: 300;  
+}
+.small {
+  margin: 0;
+  font-size: 14px;
+  font-weight: 300;
+}
+
+.work-container {
+  background-color:  hsl(15, 100%, 70%);
+}
+.play-container {
+  background-color: hsl(195, 74%, 62%);
+}
+.study-container {
+  background-color:  hsl(348, 100%, 68%);
+}
+.excercise-container {
+  background-color: hsl(145, 58%, 55%);
+}
+.social-container {
+  background-color: hsl(264, 64%, 52%);
+}
+.selfCare-container {
+  background-color: hsl(43, 84%, 65%);
+}
+
+@media screen and (max-width: 425px) {
+  .general-container{
+    width: 100%;
+    height: 155px;
+  }
+  .info-container {
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+  .title-container {
+    padding-top: 15px;
+  }
+  .time-information-container {
+    padding: 0 30px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 30px;
+  }
+  .big {
+    font-size: 30px;
+  }
+  .small {
+    font-size: 15px;
+  }  
+}
+</style>
+
+
+// 'work-container':props.value == work, 'play-container':props.value == play, 'study-container':props.value == study, 'excercise-container':props.value == excercise, 'social-container':props.value == social, 'self-container':props.value == selfCare
